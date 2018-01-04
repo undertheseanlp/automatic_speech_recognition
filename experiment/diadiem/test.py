4f85877f70e09fdee119fcf459002ea97926aa6a
rain <brother.rain.1024@gmail.com>
2018-01-04 16:34:28 +0700

    add wer

--- experiment/diadiem/test.py
+++ experiment/diadiem/test.py
@@ -12,7 +12,6 @@ def listen(file):
 
 wav_files = ["wav/test/" + f for f in os.listdir("wav/test")]
 wav_files = sorted(wav_files)
-# wav_files = ["wav/test/KHASCHSAJN007.wav"]
 for file in wav_files:
     output = listen(file)
     print("{} -> {}".format(file, output))
