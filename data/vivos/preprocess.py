deaefb1bccb4888f28757bc822a88ca33ef48781
rain <brother.rain.1024@gmail.com>
2018-01-05 15:10:03 +0700

    remove redandunt files

---
+++ data/vivos/preprocess.py
@@ -0,0 +1,78 @@
+import shutil
+from os import mkdir, walk
+from os import listdir
+from os.path import join
+
+
+def create_train_waves():
+    waves_folder = "raw/train/waves"
+    corpus_waves_folder = "corpus/train/wav"
+    try:
+        shutil.rmtree(corpus_waves_folder)
+    except:
+        pass
+    finally:
+        mkdir(corpus_waves_folder)
+    for root, dirs, files in walk(waves_folder):
+        for dir in dirs:
+            for f in listdir(join(waves_folder, dir)):
+                shutil.copy(
+                    join(waves_folder, dir, f),
+                    join(corpus_waves_folder, f))
+
+
+def create_test_waves():
+    waves_folder = "raw/test/waves"
+    corpus_waves_folder = "corpus/test/wav"
+    try:
+        shutil.rmtree(corpus_waves_folder)
+    except:
+        pass
+    finally:
+        mkdir(corpus_waves_folder)
+    for root, dirs, files in walk(waves_folder):
+        for dir in dirs:
+            for f in listdir(join(waves_folder, dir)):
+                shutil.copy(
+                    join(waves_folder, dir, f),
+                    join(corpus_waves_folder, f))
+
+
+def create_train_text():
+    lines = open("raw/train/prompts.txt").read().splitlines()
+    output = []
+    for line in lines:
+        items = line.split()
+        fileid = items[0]
+        text = " ".join(items[1:]).lower()
+        content = "{}|{}".format(fileid, text)
+        output.append(content)
+    text = "\n".join(output)
+    open("corpus/train/text", "w").write(text)
+
+
+def create_test_text():
+    lines = open("raw/test/prompts.txt").read().splitlines()
+    output = []
+    for line in lines:
+        items = line.split()
+        fileid = items[0]
+        text = " ".join(items[1:]).lower()
+        content = "{}|{}".format(fileid, text)
+        output.append(content)
+    text = "\n".join(output)
+    open("corpus/test/text", "w").write(text)
+
+
+try:
+    shutil.rmtree("corpus")
+except:
+    pass
+finally:
+    mkdir("corpus")
+    mkdir("corpus/train")
+    mkdir("corpus/test")
+    create_train_waves()
+    create_test_waves()
+    create_train_text()
+    create_test_text()
--- experiment/diadiem/init.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system("rm -rf etc")
-os.system("rm -rf logdir")
-os.system("rm -rf *.html")
-os.system("rm -rf wav")
-os.system("sphinxtrain -t diadiem setup")
