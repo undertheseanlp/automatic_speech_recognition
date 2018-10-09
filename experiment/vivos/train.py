c97b352112a2cf9be7fa9495987395813935d6ed
HungLe <lephihungch@gmail.com>
2018-10-10 00:08:41 +0700

    Update predict for kaldi model

--- experiment/vivos/train.py
+++ experiment/vivos/train.py
@@ -2,9 +2,11 @@ from experiment.vivos.extension.model import KaldiSpeechRecognition
 from experiment.vivos.extension.export import SphinxSpeechRecognitionExporter
 from experiment.vivos.load_data import corpus_folder
 from os.path import join, dirname
+import os
+
 
 export_folder = join(dirname(__file__), "model")
-kaldi_folder = "/Users/dkht/Downloads/kaldi-trunk"
+kaldi_folder = "/Users/rj/Downloads/kaldi-trunk"
 params = {
     "method": "deltadelta",
     "jobs": 1,
@@ -13,5 +15,5 @@ params = {
 model = KaldiSpeechRecognition(corpus_folder, kaldi_folder, params)
 model.fit()
 # SphinxSpeechRecognitionExporter.export(model, export_folder)
-# wav_file = join(tmp_folder, "etc", "wav", "train", "test", "CAFPHEE003.wav")
-# model.predict(wav_file)
+wav_file = os.path.abspath("test/VIVOSDEV01_R003.wav")
+model.predict(wav_file)
