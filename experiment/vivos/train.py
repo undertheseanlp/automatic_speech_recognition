0e498f73fa9f4b5b2ce9f8e02338614f78512b71
RJ <lephihungch@gmail.com>
2018-10-19 13:53:34 +0700

    Update agrument for train.py

--- experiment/vivos/train.py
+++ experiment/vivos/train.py
@@ -1,15 +1,28 @@
 from experiment.vivos.extension.model import KaldiSpeechRecognition
-from experiment.vivos.load_data import corpus_folder
 from os.path import join, dirname
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--kaldi_folder', help='Kaldi dir path', required=True)
+parser.add_argument('--corpus_folder', help='Corpus path to train',required=True)
+parser.add_argument('--export_path', help='Export path will be able soon')
+parser.add_argument('--nj', help='Parallel number of job', default=1)
+parser.add_argument('--method', help='Parallel number of job', default="deltadelta")
 
 
-export_folder = join(dirname(__file__), "model")
-kaldi_folder = "/Users/rj/Downloads/kaldi-trunk"
-params = {
-    "method": "deltadelta",
-    "jobs": 1,
-    "lm_order": 1
-}
-model = KaldiSpeechRecognition(corpus_folder, kaldi_folder, params)
-model.fit()
+args = parser.parse_args()
 
+
+def train(kaldi_folder, corpus_folder, export_folder=None, nj=1, method="deltadelta"):
+    export_folder = join(dirname(__file__), "model")
+    params = {
+        "method": method,
+        "jobs": nj,
+        "lm_order": 1
+    }
+    model = KaldiSpeechRecognition(corpus_folder, kaldi_folder, params)
+    model.fit()
+
+
+if __name__ == "__main__":
+    train(args.kaldi_folder, args.corpus_folder, args.export_path,args.nj,args.method)
